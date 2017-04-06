e0f10c60151d94fbbc53bc098f384282be6a4f9e
MeatMAN <skylineflash@rambler.ru>
2017-04-06 20:30:09 +0300

    initial

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+	<p>&copy; MeatMAN {{ date('Y') }}</p>
+</footer>
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>VK Top Users Charts @yield('head-title')</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<!-- Le styles -->
+		<link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+		<style type="text/css">
+			body {
+				padding-top: 60px;
+				padding-bottom: 40px;
+			}
+		</style>
+		<link href="/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
+		<link rel="shortcut icon" href="/assets/ico/favicon.ico">
+	</head>
+
+	<body>
+
+		@include('layouts.nav')
+
+		<div class="container">
+			@yield('content')			
+
+			<hr>
+
+			@include('layouts.footer')
+
+		</div> <!-- /container -->
+		@yield('scripts')
+	</body>
+</html>
